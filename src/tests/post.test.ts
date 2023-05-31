c1276a951e39c0c08e4dfe66f30df34f1b7bf21d
otaru-rich <otarurich@gmail.com>
2023-06-01 00:07:25 +0100

    TEST: Added post CRUD, like and unlike end-points tests

---
+++ src/tests/post.test.ts
@@ -0,0 +1,98 @@
+import request from 'supertest';
+import { connectDB, disconnectDB } from '../config/database'
+import * as User from '../services/user.services'
+import { app, server } from '..'
+import {describe, expect, test, beforeEach, afterEach} from '@jest/globals';
+import { IUser } from '../models/user.model'
+import { getPostsByUserId } from '../services/post.service'
+
+
+/* Connecting to the database before each test. */
+beforeEach(async () => {
+  await connectDB()
+});
+
+/* Closing database connection after each test. */
+afterEach(async () => {
+  await disconnectDB();
+  await new Promise((resolve) => {
+    server.close(resolve);
+  });
+});
+
+describe('Post API', () => {
+  let token: string = '';
+  let user: IUser ;
+  beforeAll(async () => {
+    /** Simulate user authentication */
+    const agent = request.agent(app);
+    const response = await agent.post('/api/v1/users/login').send({
+      email: 'test@example.com',
+      password: 'password123',
+    });
+    token = response.body.token;
+    user = await  User.getUserByEmail('test@example.com') as IUser;
+  });
+
+  // Test case for creating a new post
+  test('should create a new post', async () => {
+    const response = await request(app)
+      .post('/api/v1/posts')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Test Post',
+        content: 'This is a test post.',
+        userId: `${user._id}`
+      });
+
+    expect(response.status).toBe(201);
+    expect(response.body.data).toHaveProperty('title', 'Test Post');
+    expect(response.body.data).toHaveProperty('content', 'This is a test post.');
+  });
+
+  // Test case for retrieving user posts
+  test('should retrieve user posts', async () => {
+    const response = await request(app)
+      .get('/api/v1/posts')
+      .set('Authorization', `Bearer ${token}`)
+      .send({userId: user._id});
+
+    expect(response.status).toBe(200);
+    expect(response.body.data).toBeInstanceOf(Array);
+  });
+
+  // Test case for retrieving posts from followed users
+  test('should retrieve posts from followed users', async () => {
+    const response = await request(app)
+      .get('/api/v1/posts/following')
+      .set('Authorization', `Bearer ${token}`)
+      .send({userId: user._id});
+
+    expect(response.status).toBe(200);
+    expect(response.body.data).toBeInstanceOf(Array);
+  });
+
+  // Test case for liking a post
+  test('should like a post', async () => {
+    const posts = await getPostsByUserId(user._id.toString())
+    const response = await request(app)
+      .post(`/api/v1/posts/${posts[0]._id}/like`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({userId: user._id});
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('message', 'Post liked successfully');
+  });
+
+  // Test case for unliking a post
+  test('should unlike a post', async () => {
+    const posts = await getPostsByUserId(user._id.toString())
+    const response = await request(app)
+      .delete(`/api/v1/posts/${posts[0]._id}/like`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({userId: user._id});
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('message', 'Post unliked successfully');
+  });
+});
